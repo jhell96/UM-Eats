2a61dfbd4d471bffd594e06704501205665a4dfd
Josh <jhellerstein96@gmail.com>
2017-01-22 02:16:40 -0500

    updated

--- UM-Eats-IOS-App/js/controller/SugPageController.js
+++ UM-Eats-IOS-App/js/controller/SugPageController.js
@@ -1,10 +1,14 @@
 /*jslint browser: true*/
 /*global console, MyApp*/
 
-MyApp.angular.controller('RestPageController', ['$scope', '$http', 'InitService', 'DataService', 
+MyApp.angular.controller('SugPageController', ['$scope', '$http', 'InitService', 'DataService', 
 	function ($scope, $http, InitService, DataService) {
   		'use strict';
- 
+  
+    DataService.addEventListener('sugClicked', function(restKey) {
+      $scope.restKey = restKey;
+    });
+
     DataService.getData().then(function(result) {
     	$scope.categories = result.data.categories;
     	$scope.restaurants = result.data.restaurants;
--- UM-Eats-IOS-App/js/mods.js
+++ UM-Eats-IOS-App/js/mods.js
@@ -8,3 +8,37 @@ function unhide() {
 function mapLoad() {
 	document.getElementById("map").style.height = "100%";
 }
+
+function loadSugPage(ele) {
+    var link = ele.getAttribute("data-url");
+    console.log(link);
+    var iframe = '<iframe width="100%" height="800" src="'+link+'" frameborder="0" embedded="true"></iframe>';
+    
+    $('#pop-preloader-sug').show();
+	$('#popup-embed-sug').hide();
+    $('#popup-embed-sug').html(iframe);
+	    
+    setTimeout(function() {
+	    $('#pop-preloader-sug').hide();
+    	$('#popup-embed-sug').show();
+	}, 500);
+} 
+
+// function loadMenuPage(ele) {
+//     var link = ele.getAttribute("data-url");
+//     console.log(link);
+//     var iframe = '<iframe width="100%" height="800" src="'+link+'" frameborder="0" embedded="true"></iframe>';
+
+//     if (link.indexOf(".pdf") !== -1) {
+//     	PDFObject.embed(link, "#popup-embed-menu");
+//     }
+    
+//     $('#pop-preloader-menu').show();
+// 	$('#popup-embed-menu').hide();
+//     $('#popup-embed-menu').html(iframe);
+	    
+//     setTimeout(function() {
+// 	    $('#pop-preloader-menu').hide();
+//     	$('#popup-embed-menu').show();
+// 	}, 500);
+// }
